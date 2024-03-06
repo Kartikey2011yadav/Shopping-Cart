82b3f4fe6631f9d65a59196a3361849134ade8b4
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-06 08:04:03 +0530

    update

---
+++ src/Login.java
@@ -0,0 +1,26 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+//@WebServlet("/")
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String usr = req.getParameter("user");
+        String pass = req.getParameter("password");
+        System.out.println(usr+" "+pass);
+
+    }
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException
+    {
+
+        doGet(request, response);
+    }
+}
