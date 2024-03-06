f867ea7a449f3d304fddf2675bf7974fd09f7830
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-06 08:58:54 +0530

    update login

--- src/Login.java
+++ src/Login.java
@@ -7,13 +7,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-//@WebServlet("/")
+@WebServlet("/login-main")
 public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String usr = req.getParameter("user");
         String pass = req.getParameter("password");
         System.out.println(usr+" "+pass);
+        if(usr.equals("admin") && pass.equals("1234")){
+            resp.setStatus(resp.SC_ACCEPTED);
+            resp.setHeader("Location", "/Shopping-Cart/");
+            resp.sendRedirect("/Shopping-Cart/");
+            return;
+        }
 
     }
     protected void doPost(HttpServletRequest request,
