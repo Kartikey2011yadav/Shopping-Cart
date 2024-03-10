578e1d33e0d0758aa4f48bcf8ac558d251837763
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-10 20:07:07 +0530

    Update create Account

--- src/main/CreateAccount.java
+++ src/main/CreateAccount.java
@@ -1,3 +1,5 @@
+package main;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -6,13 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-
-@WebServlet("/login-main")
-public class Login extends HttpServlet {
+@WebServlet("create-Account")
+public class CreateAccount extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String usr = req.getParameter("user");
         String pass = req.getParameter("password");
+        String confPass = req.getParameter("conf-password");
         System.out.println(usr+" "+pass);
         if(usr.equals("admin") && pass.equals("1234")){
             resp.setStatus(resp.SC_ACCEPTED);
@@ -22,6 +24,7 @@ public class Login extends HttpServlet {
         }
 
     }
+    @Override
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
             throws ServletException, IOException
